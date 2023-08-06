432da79112935fd363073519ab9c754be87e8e2a
itbebop <it.bebop@gmail.com>
2023-08-06 23:59:57 +0900

    2023.08.06. filter 설정 중

--- src/main/java/com/bebop/config/SecurityConfig.java
+++ src/main/java/com/bebop/config/SecurityConfig.java
@@ -8,6 +8,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import com.filter.MyFilter1;
+import com.filter.MyFilter2;
+
 import lombok.RequiredArgsConstructor;
 
 @Configuration // ioc
@@ -20,12 +24,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         // http.csrf().disable();
+        // http.addFilter(new MyFilter1());
+        // http.addFilter(new MyFilter2());
+        // http.addFilterBefore(new MyFilter1(), BasicAuthenticationFilter.class);
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
                 .addFilter(corsFilter) // cors 정책에서 벗어나서 cross 요청이 와도 다 허용이 됨(인증이 필요하지 않은 요청에 한하여)
                                        // @CroosOrigin(인증없을때)/ 시큐리티 필터에 등록해야(인증있을때)
                 .formLogin().disable()
-                .httpBasic().disable()
+                .httpBasic().disable() // httpBasic이란 Authorization에 id, pw달고 가는 방법
+                // Bearer는 Token 달고 가는 방식으로 이 Bearer를 쓸 것이므로 httpBasic은 disable하는 것
                 .authorizeRequests()
                 .antMatchers("/api/v1/user/**")
                 .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
---
+++ src/main/java/com/filter/MyFilter1.java
@@ -0,0 +1,31 @@
+package com.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyFilter1 implements Filter {
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse res = (HttpServletResponse) response;
+
+        if (req.getMethod().equals("POST")) {
+            String headerAuth = req.getHeader("Authorization");
+            System.out.println("headerAuth");
+        }
+
+        System.out.println("===================필터1");
+        chain.doFilter(req, res);
+    }
+
+}
